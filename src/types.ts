62cd85782dca5fe6a00b79f5156fd90a22c9159c
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2023-06-23 11:46:34 +0200

    add new type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import * as HTMLParser from  "node-html-parser"
+
 export type HTMLScrapOpt = {
     includeStyle: boolean,
     includeScript: boolean,
@@ -5,6 +7,11 @@ export type HTMLScrapOpt = {
 
 export type TemplateSetOpt = {
     template: templatesList
+}
+
+export type StylesTagObj = {
+    stylesTag: HTMLParser.HTMLElement[],
+    linkStyleTag: HTMLParser.HTMLElement[]
 }
 
 type templatesList = 'html' | 'html-js' | 'html-css' | 'html-css-js'
